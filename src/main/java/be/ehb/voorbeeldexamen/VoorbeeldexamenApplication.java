e4078c6b5bde79080908a4154b52777a626b4337
david.van.steertegem@gmail.com <PowerUser420>
2021-06-19 16:10:40 +0200

    initial commit, added project structure

---
+++ src/main/java/be/ehb/voorbeeldexamen/VoorbeeldexamenApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.voorbeeldexamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoorbeeldexamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoorbeeldexamenApplication.class, args);
+    }
+
+}
